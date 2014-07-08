66aae58754a6072e20ac600c969b68b1d09d4e2a
Ross Nelson <ross.nelson@gmail.com>
2014-07-08 18:54:51 -0500

    Initial Commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Ross Nelson on 7/8/14.
+//  Copyright (c) 2014 Food Inspections. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Ross Nelson on 7/8/14.
+//  Copyright (c) 2014 Food Inspections. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
